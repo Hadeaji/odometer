7c60cdb0c7141757f00a20de78a05a94fb7a379b
Hadi Aji <hadeaji@gmail.com>
2021-01-18 15:58:08 +0200

    lab37

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Header(props) {
+  return (
+    <div className="App">
+    <h1>Greeting {props.text}</h1>
+    </div>    
+  );
+}
+
+class Odometer extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      counter: 0
+    };
+  }
+
+  addons(value){
+    if (this.state.counter+value <= 9999) {
+      this.setState({
+        counter: this.state.counter+value
+      });
+    }
+    else{
+      this.setState({
+        counter: this.state.counter+value-10000
+      });
+    }
+  }
+
+  render(){
+    return(
+      <>
+        <div className="App">
+          <button onClick={()=> this.addons(1)}>ADD 1</button>
+          <button onClick={()=> this.addons(10)}>ADD 10</button>
+          <button onClick={()=> this.addons(100)}>ADD 100</button>
+          <button onClick={()=> this.addons(1000)}>ADD 1000</button>
+        </div>
+
+        <div className="App">
+          <h2>Your Odometer</h2>
+          <h2>{String(this.state.counter).padStart(4, 0)}</h2>
+        </div>
+      </>
+    )
+  }
+}
+
+function Footer(props) {
+  return (
+    <div className="App">
+    <h5>#feel_free_to_rest_here {props.trademark} </h5>
+    </div>    
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header text="Traveler"/>
+    <Odometer />
+    <Footer trademark="@copyright reserved"/>
+    </>
   );
 }
 
